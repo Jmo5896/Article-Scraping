931731b431e001ff03a646aae1fed0ca4fa5f52e
jmo5896 <justin.m.moore5896@gmail.com>
2019-12-27 11:34:40 -0600

    not sure what was added

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/OnionArticles"
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true});
 
 //routes
 require('./routes/html_routes.js')(app);
